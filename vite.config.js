3b27b3995e994559bc2c49529fc71e27f2a8ec83
paualberobatoi <alberomorapau.batoi@gmail.com>
2026-01-12 20:06:01 +0100

    Vista con el css bien

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
-import path from 'path'; // <--- Añade esto
 
 export default defineConfig({
     plugins: [
-        tailwindcss(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    // --- AÑADE ESTO PARA FORZAR LA RUTA ---
-    resolve: {
-        alias: {
-            'tailwindcss': path.resolve(__dirname, 'node_modules/tailwindcss'),
-        },
-    },
 });
